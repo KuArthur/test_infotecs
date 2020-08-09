22aee8ab88fb3d14cf2673ae57e232dd5ca8c03e
KuArthur <48932889+KuArthur@users.noreply.github.com>
2020-08-09 06:15:35 +0300

    настроил корректный вывод данных на страницу

--- app.js
+++ app.js
@@ -1,30 +1,73 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
+const url = require('url');
+
 
 const users = require('./js/test.js')
 
-http.createServer(function(req,res) {
-    fs.readFile(__dirname + '/view/test.html','utf-8', (err,data) => {
+const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json'
+};
+
+http.createServer((req,res) => {
+    let filePath = '.' + req.url;
+//при запуске сервера переход на index.html
+    if (filePath == './') {
+        filePath = './index.html';
+    }
+    
+    fs.readFile(filePath,'utf-8', (err,data) => {
         if(err) throw err;
-        let rows;
+        let rows = '';
         for (let user of users) {
-            rows += '<tr><td>' + user.name.firstName + '</td><td>' + user.name.lastName + '</td></tr>'+ '</td><td>' + user.about + '</td></tr>' + '</td><td>' + user.eyeColor + '</td></tr>';
-            
+            rows += '<tr class ="table__row"><td class ="table__column">' + user.name.firstName + '</td><td class ="table__column">' + user.name.lastName + '</td>'+ '<td class ="table__column table__about">' + user.about + '</td>' + '<td class ="table__column">' + user.eyeColor + '</td></tr>';
         }
         
-       data = data.replace('{row}',rows);
-        //users.forEach(el => data.replace('{row}', '<tr><td>' + el.name.firstName + '</td><td>' + el.name.lastName + '</td></tr>'+ '</td><td>' + el.about + '</td></tr>' + '</td><td>' + el.eyeColor + '</td></tr>'));
+        data = data.replace('{row}',rows);
+
         res.end(data);
-        // console.log(data);
     })
 
-     //req.url has the pathname, check if it conatins '.css'
-
-    //   fs.readFile(__dirname + '/public/test.css', (err, data) => {
-    //     if(err) throw err;
-    //     res.writeHead(200, {'Content-Type': 'text/css'});
-    //     res.write(data);
-    //     res.end();
-    //   });
-
 }).listen(3000)
+//TODO: в чем различие кода ниже и выше?
+// http.createServer(function (req, res) {
+//     console.log('req ', req.url);
+// //при запуске сервера переход на index.html
+//     let filePath = '.' + req.url;
+//     if (filePath == './') {
+//         filePath = './index.html';
+//     }
+// //получаем расширение файла из его ссылки
+//     // let extname = String(path.extname(filePath)).toLowerCase();
+
+//     // let contentType = mimeTypes[extname];
+    
+//     // if(filePath.indexOf('index.html',-1)) {
+//         fs.readFile(filePath, (err,data) => {
+//             if(err) throw err;
+//             let rows = '';
+//             for (let user of users) {
+//                 rows += '<tr><td>' + user.name.firstName + '</td><td>' + user.name.lastName + '</td>'+ '<td>' + user.about + '</td>' + '<td>' + user.eyeColor + '</td></tr>';
+//             }
+            
+//             data = data.replace('{row}',rows);
+
+//             res.end(data);
+//         })
+//     // }
+   
+//     // fs.readFile(filePath, (err, data) => {
+//     //     // обработка ошибок
+//     //     if(err) throw err
+//     //     // пишем в заголовок HTTP-код, говорящий о том,что страница успешно загружена
+//     //     // res.writeHead(200, { 'Content-Type': contentType });
+        
+//     //      res.end(data, 'utf-8');
+    
+//     // });
+
+// }).listen(3000);
--- js/test.js
+++ js/test.js
@@ -1,35 +1,9 @@
 //const table = document.querySelector('.table');
 const fs = require('fs')
 // const users = require('../user.json'); не стоит использовать т.к. файл будет прочитан только один раз
-// function readerFile(filepath,callback) {
-//     fs.readFile(filepath, (err,data) => {
-//         try {
-//             const obj = JSON.parse(data);
-//             callback && callback(null,obj);
-//         }
-//         catch {
-//             return callback && callback(err);
-//         }
-//     })
-// }
-
-
-
-// readerFile("../test/user.json", (err,data) => { // почему не работает ../user.json
-//     if(err) throw err;
-//     //users.forEach((el,i) => table.appendChild('<tr><td>' + el.name.firstName + '</td><td>' + el.name.lastName + '</td></tr>'+ '</td><td>' + el.about + '</td></tr>' + '</td><td>' + el.eyeColor + '</td></tr>'))
-//    // console.log(data);
-//     return data;
-// })
-
-// console.log(users);
-
-//users.forEach((el,i) => table.appendChild('<tr><td>' + el.name.firstName + '</td><td>' + el.name.lastName + '</td></tr>'+ '</td><td>' + el.about + '</td></tr>' + '</td><td>' + el.eyeColor + '</td></tr>'))
 
 let users = fs.readFileSync("../test/user.json", 'utf-8')
 
 users = JSON.parse(users)
 module.exports = users;
-//let contain = users.forEach((el,i) => table.appendChild('<tr><td>' + el.name.firstName + '</td><td>' + el.name.lastName + '</td></tr>'+ '</td><td>' + el.about + '</td></tr>' + '</td><td>' + el.eyeColor + '</td></tr>'))
-//let contain = '<tr><td>' + users.name.firstName + '</td><td>' + users.name.lastName + '</td></tr>'+ '</td><td>' + users.about + '</td></tr>' + '</td><td>' + users.eyeColor + '</td></tr>'
-//module.exports = contain;
+
